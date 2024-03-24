51defbc3134fd56098debc3e3a6222fcd29b69a7
Yuechen Jiang <57229766+Carolyn-Jiang@users.noreply.github.com>
2024-03-24 17:41:52 -0400

    Update run_tgi.sh

--- run_tgi.sh
+++ run_tgi.sh
@@ -10,12 +10,12 @@ export OPENAI_API_KEY="Enter your OpenAI API Key here"
 # -et 2022-09-22 \
 # -rm train \
 # -cp config/tsla_tgi_config.toml \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
 # -rp data/05_train_model_output
 # # train-checkpoint
 # python run.py sim-checkpoint \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
 # -rp data/05_train_model_output \
 # -cp config/tsla_tgi_config.toml \
 # -rm train
@@ -28,12 +28,12 @@ export OPENAI_API_KEY="Enter your OpenAI API Key here"
 # -et 2023-04-10 \
 # -rm test \
 # -cp config/tsla_gpt_config.toml \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
 # -rp data/05_train_model_output
 # # test-checkpoint
 # python run.py sim-checkpoint \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
-# -ckp /workspace/ofcourseistillloveyou-exp/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
+# -ckp /workspace/FinMem-LLM-StockTrading/data/06_train_checkpoint \
 # -rp data/05_train_model_output \
 # -cp config/tsla_gpt_config.toml \
 # -rm test
